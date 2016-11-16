d0538f9a5c1e42e15d9f99d11d02bb7e8f225e74
Roland Maio <rolandmaio38@gmail.com>
2016-11-16 10:05:47 -0500

    - Added views for viewing the details of a DishPost, for a Diner ordering   a DishPost and for a Diner viewing their orders. - Modified the index view to return a list of the DishPosts.

--- django-project/dishes/views.py
+++ django-project/dishes/views.py
@@ -1,5 +1,33 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+
+from dishes.models import *
 
 def index(request):
-    return HttpResponse("Hello, word. You're at the dishes index.")
+    dish_posts = DishPost.objects.all()
+    context = {"dish_posts": dish_posts}
+    return render(request, "dishes/index.html", context)
+
+def post_detail(request, dish_post_id):
+    dish_post = get_object_or_404(DishPost, pk=dish_post_id)
+    context = {"dish_post": dish_post}
+    if request.user.is_authenticated:
+        context["user"] = request.user
+    return render(request, "dishes/post_detail.html", context)
+
+def order_dish(request, dish_post_id):
+    diner = Diner.objects.get(user=request.user)
+    dish_post = DishPost.objects.get(pk=dish_post_id)
+    num_servings = request.POST["num_servings"]
+    order = Order.objects.create(diner=diner,
+                                 dish_post=dish_post,
+                                 num_servings=num_servings)
+    return redirect("orders")
+
+def orders(request):
+    if not request.user.is_authenticated:
+        return redirect("dishes")
+    else:
+        diner = Diner.objects.get(user=request.user)
+        orders = Order.objects.filter(diner=diner)
+        context = {"orders": orders}
+        return render(request, "dishes/orders.html", context)
