bb84ff0d4b859bd1d81d3de2e08edb253c06249a
Roland Maio <rolandmaio38@gmail.com>
2016-11-16 10:04:34 -0500

    Added urls for viewing the details of a DishPost, viewing the orders a Diner has placed and for placing an order of a DishPost.

--- django-project/dishes/urls.py
+++ django-project/dishes/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.index, name="index"),
+    url(r"^orders/$", views.orders, name="orders"),
+    url(r"^posts/(?P<dish_post_id>[0-9]+)/$", views.post_detail),
+    url(r"^posts/(?P<dish_post_id>[0-9]+)/order/$", views.order_dish),
 ]
