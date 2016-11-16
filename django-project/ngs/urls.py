d1aaafe5b8ea71083ba554b6f7afc52a5723904f
Roland Maio <rolandmaio38@gmail.com>
2016-11-16 11:37:00 -0500

    Added kwargs argument to logout url which contains the key-value pair "next_page"-"/dishes/". This redirects the user to the dishes url after they are successfully logged out.

--- django-project/ngs/urls.py
+++ django-project/ngs/urls.py
@@ -19,7 +19,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r"^login/$", auth_views.login, name="login"),
-    url(r"^logout/$", auth_views.logout, name="logout"),
+    url(r"^logout/$",
+        auth_views.logout,
+        name="logout",
+        kwargs={"next_page": "/dishes/"}),
     url(r"^dishes/", include("dishes.urls")),
     url(r'^admin/', admin.site.urls),
 ]
