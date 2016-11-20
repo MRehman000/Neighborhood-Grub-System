37c63e12a0ae36c9783294a397dee8ddce2daff9
Roland Maio <rolandmaio38@gmail.com>
2016-11-20 17:56:44 -0500

    - Added urls for dish requests page and dish request details page. - In the prototype, the index page showed all the dish posts. To   differentiate between dish posts and dish requests this page is now   the posts page and there is a link to it from the site home page.

--- django-project/dishes/urls.py
+++ django-project/dishes/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.index, name="index"),
     url(r"^orders/$", views.orders, name="orders"),
+    url(r"^posts/$", views.posts),
     url(r"^posts/(?P<dish_post_id>[0-9]+)/$", views.post_detail),
     url(r"^posts/(?P<dish_post_id>[0-9]+)/order/$", views.order_dish),
+    url(r"^requests/$", views.requests),
+    url(r"^requests/(?P<dish_request_id>[0-9]+)/$", views.request_detail),
 ]
