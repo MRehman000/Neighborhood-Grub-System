b928dcc1f861e98a60df9cf09a890e6814fd61a1
Roland Maio <rolandmaio38@gmail.com>
2016-11-19 21:01:24 -0500

    Removed requests app and added accounts app.

--- django-project/ngs/settings.py
+++ django-project/ngs/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "dishes",
-    "requests",
+    "accounts",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
