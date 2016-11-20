fa611e96d0e09ddf3fd5852ef6d64606788de497
Roland Maio <rolandmaio38@gmail.com>
2016-11-20 00:13:59 -0500

    Added form for Chef Permissions Request.

--- django-project/accounts/forms.py
+++ django-project/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from accounts.models import CreateAccountRequest
+from accounts.models import CreateAccountRequest, ChefPermissionsRequest
 
 class CreateAccountRequestForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,16 @@ class TerminateAccountRequestForm(forms.Form):
     NO = 0
     YES = 1
     choice = forms.fields.ChoiceField(choices=((YES, "Yes"), (NO, "No")))
+
+class ChefPermissionsRequestForm(forms.ModelForm):
+    class Meta:
+        model = ChefPermissionsRequest
+        fields = [
+            "first_dish_name",
+            "first_dish_image",
+            "second_dish_name",
+            "second_dish_image",
+            "third_dish_name",
+            "third_dish_image",
+            "video_biography"
+        ]
