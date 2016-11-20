c6bc5fb5ff6e71fe36c9c6127c70a30a9980511a
Roland Maio <rolandmaio38@gmail.com>
2016-11-20 00:16:39 -0500

    Added url fo request chefs permissions use case.

--- django-project/accounts/urls.py
+++ django-project/accounts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r"^$", views.account, name="account"),
     url(r"^signup/$", views.signup, name="signup"),
     url(r"^terminate/$", views.terminate, name="request-terminate"),
+    url(r"^request-chef-permissions/$",
+        views.request_chef_permissions,
+        name="request-chef-permissions"),
 ]
