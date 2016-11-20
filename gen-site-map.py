943ded68b79f0161086471b3181bd2be722c03e8
Roland Maio <rolandmaio38@gmail.com>
2016-11-20 16:30:45 -0500

    - Added dependency on pydot 1.2.3 to requirements.txt. This is for   rendering the site-map. - Added site-map.png to the .gitignore file. This is likely to change   quite a bit and the overhead of running the script to generate the   site-map is worth avoiding the headache of keeping track of versions   of the site-map script. - Added script to generate site-map, gen-site-map.py.

---
+++ gen-site-map.py
@@ -0,0 +1,26 @@
+import json
+
+import pydot
+
+edges = (
+    ("home", "login"),
+    ("home", "logout"),
+    ("home", "dish posts"),
+    ("home", "dish requests"),
+    ("home", "account"),
+    ("login", "signup"),
+    ("login", "home")
+)
+
+def main():
+
+    graph = pydot.Dot(graph_type="digraph")
+
+    for u, v in edges:
+        edge = pydot.Edge(u, v)
+        graph.add_edge(edge)
+    else:
+        graph.write_png("site-map.png")
+
+if __name__ == "__main__":
+    main()
