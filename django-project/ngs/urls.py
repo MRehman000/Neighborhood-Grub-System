aa6147cfa4feadf38f1b770a8feb2691155f24f2
Roland Maio <rolandmaio38@gmail.com>
2016-11-19 19:09:47 -0500

    - Added url for web site home page. - Added url for signing up, that is for creating a create account   request.

--- django-project/ngs/urls.py
+++ django-project/ngs/urls.py
@@ -17,12 +17,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from ngs import views
+from requests import views as req_views
+
 urlpatterns = [
+    url(r"^$", views.index, name="index"),
     url(r"^login/$", auth_views.login, name="login"),
     url(r"^logout/$",
         auth_views.logout,
         name="logout",
         kwargs={"next_page": "/dishes/"}),
+    url(r"^signup/$", req_views.signup, name="signup"),
     url(r"^dishes/", include("dishes.urls")),
     url(r'^admin/', admin.site.urls),
 ]
