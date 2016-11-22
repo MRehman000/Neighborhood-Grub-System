83a58ba794290882968b17b1b18da63dfd10f9ad
Roland Maio <rolandmaio38@gmail.com>
2016-11-21 22:46:03 -0500

    Added url for create a request view

--- django-project/dishes/urls.py
+++ django-project/dishes/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r"^posts/(?P<dish_post_id>[0-9]+)/$", views.post_detail),
     url(r"^posts/(?P<dish_post_id>[0-9]+)/order/$", views.order_dish),
     url(r"^requests/$", views.requests),
+    url(r"^requests/create/$", views.create_request),
     url(r"^requests/(?P<dish_request_id>[0-9]+)/$", views.request_detail),
     url(r"^chefs/(?P<chef_id>[0-9]+)/$", views.chef_detail),
 ]
