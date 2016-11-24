487d6b1b95846556db2c970ebd0d6cdbf061ccc9
Md Rahman <mdrahman1472@yahoo.com>
2016-11-24 16:01:36 -0500

    static file created, setting changes for static file and background photo added to login page

--- django-project/ngs/settings.py
+++ django-project/ngs/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Redirect after successful login
 LOGIN_REDIRECT_URL = "/"
