00bc84a50c43321efcce55b7a0b012287896e3e5
Roland Maio <rolandmaio38@gmail.com>
2016-12-01 17:46:55 -0500

    Added url for edit post page

--- django-project/dishes/urls.py
+++ django-project/dishes/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r"^posts/(?P<dish_post_id>[0-9]+)/$", views.post_detail),
     url(r"^posts/(?P<dish_post_id>[0-9]+)/order/$", views.order_dish),
     url(r"^posts/(?P<dish_post_id>[0-9]+)/cancel/$", views.cancel_post),
+    url(r"^posts/(?P<dish_post_id>[0-9]+)/edit/$", views.edit_post),
     url(r"^requests/$", views.requests),
     url(r"^requests/create/$", views.create_request),
     url(r"^requests/(?P<dish_request_id>[0-9]+)/$", views.request_detail),
