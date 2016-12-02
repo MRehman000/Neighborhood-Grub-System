3363b493003c220732c66fc8338392778ea2fb97
Roland Maio <rolandmaio38@gmail.com>
2016-12-01 20:01:23 -0500

    Added ModelAdmin class for ChefPermissionsRequest. The ModelAdmin class has the actions grant_request and deny_request. These need to be fleshed out.

--- django-project/accounts/admin.py
+++ django-project/accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from accounts.models import ChefPermissionsRequest
 
-# Register your models here.
+@admin.register(ChefPermissionsRequest)
+class ChefPermissionsRequestAdmin(admin.ModelAdmin):
+    """
+    Django ModelAdmin class for providing the Grant/Deny Chef Permissions
+    Request functionality.
+    """
+    actions = ["grant_request", "deny_request"]
+
+    def grant_request(self, request, queryset):
+        print("What what what what")
+
+    def deny_request(self, request, queryset):
+        print("Yut yut yut yut")
