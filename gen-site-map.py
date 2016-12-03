a8f92c66cd0392e6b5ee2356f2d41ab279acff47
Roland Maio <rolandmaio38@gmail.com>
2016-12-02 20:56:00 -0500

    Added tuple of 2-tuples that contain page html-template-path pairs.

--- gen-site-map.py
+++ gen-site-map.py
@@ -34,6 +34,27 @@ edges = (
     ("create post", "orders-requests"),
 )
 
+page_to_template_path = (
+    ("home",            "ngs/django-project/ngs/templates/ngs/index.html"),
+    ("login",           "ngs/django-project/ngs/templates/registration/login.html"),
+    ("account",         "ngs/django-project/accounts/templates/accounts/account.html"),
+    ("suggestion",      "ngs/django-project/accounts/templates/accounts/suggestion.html"),
+    ("signup",          "ngs/django-project/accounts/templates/accounts/signup.html"),
+    ("dish posts",      "ngs/django-project/dishes/templates/dishes/posts.html"),
+    ("dish requests",   "ngs/django-project/dishes/templates/dishes/requests.html"),
+    ("post detail",     "ngs/django-project/dishes/templates/dishes/post_detail.html"),
+    ("orders-requests", "ngs/django-project/dishes/templates/dishes/orders-requests.html"),
+    ("create post",     "ngs/django-project/dishes/templates/dishes/create_post.html"),
+    ("request detail",  "ngs/django-project/dishes/templates/dishes/request_detail.html"),
+    ("cancel request",  "ngs/django-project/dishes/templates/dishes/cancel_request.html"),
+    ("edit post",       "ngs/django-project/dishes/templates/dishes/edit_post.html"),
+    ("cancel order",    "ngs/django-project/dishes/templates/dishes/cancel_order.html"),
+    ("chef profile",    "ngs/django-project/dishes/templates/dishes/chef_bio.html"),
+    ("manage posts",    "ngs/django-project/dishes/templates/dishes/manage_posts.html"),
+    ("create request",  "ngs/django-project/dishes/templates/dishes/create_post.html"),
+    ("edit request",    "ngs/django-project/dishes/templates/dishes/edit_request.html"),
+)
+
 def main():
 
     graph = pydot.Dot(graph_type="digraph")
