4743fdf05bc48a8cadb0b7832784cb8c4185a871
Roland Maio <rolandmaio38@gmail.com>
2016-12-03 18:04:30 -0500

    Added ModelAdmin for CreateAccountRequest.

--- django-project/accounts/admin.py
+++ django-project/accounts/admin.py
@@ -1,5 +1,65 @@
 from django.contrib import admin
-from accounts.models import ChefPermissionsRequest, RedFlag, Complaint
+from django.contrib.auth.models import User
+
+from accounts.models import ChefPermissionsRequest, RedFlag
+from accounts.models import CreateAccountRequest, Complaint
+
+from dishes.models import Diner
+
+@admin.register(CreateAccountRequest)
+class CreateAccountRequestAdmin(admin.ModelAdmin):
+    """
+    Django ModelAdmin class for providing the Approve/Reject Create Account
+    Request functionality.
+    """
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "status",
+        "user"
+    ]
+    actions = ["approve_request", "reject_request"]
+
+    def get_queryset(self, request):
+        """
+        Limit the CreateAccountRequest queryset to those that are pending.
+        """
+        qs = super(CreateAccountRequestAdmin, self).get_queryset(request)
+        return qs.filter(status=CreateAccountRequest.PENDING)
+
+    def approve_request(self, request, queryset):
+        """
+        Create an account for each approved Create Account Request.
+        """
+        for createAccountRequest in queryset:
+            # Create the User object.
+            # Generate a random password
+            password = User.objects.make_random_password()
+            pass_file = "{}_password.txt".format(createAccountRequest.username)
+            with open(pass_file, "w") as fh:
+                print(password, file=fh)
+            # "email" the person their random password. For this project that
+            # will simply be printing the random password to stdout
+            username = createAccountRequest.username
+            email = createAccountRequest.email
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # Create Diner object.
+            diner = Diner.objects.create(user=user)
+            # Update the status of this Create Account Request
+            createAccountRequest.status = CreateAccountRequest.APPROVED
+            createAccountRequest.save()
+
+    def reject_request(self, request, queryset):
+        """
+        Set the status of each rejected request to rejected.
+        """
+        for createAccountRequest in queryset:
+            createAccountRequest.status = CreateAccountRequest.REJECTED
+            createAccountRequest.save(update_fields=["status"])
 
 @admin.register(ChefPermissionsRequest)
 class ChefPermissionsRequestAdmin(admin.ModelAdmin):
