5bcd47ee477b5585b95ba4032aaa74b13afff528
Roland Maio <rolandmaio38@gmail.com>
2016-12-04 14:09:11 -0500

    - Changed display of ChefPermissionsRequest admin site so that the super   user can view the images and video that have been submitted. - Implemented the actions grant_request and deny_request

--- django-project/accounts/admin.py
+++ django-project/accounts/admin.py
@@ -1,10 +1,12 @@
+import os
+
 from django.contrib import admin
 from django.contrib.auth.models import User
 
 from accounts.models import ChefPermissionsRequest, RedFlag, Complaint
 from accounts.models import TerminateAccountRequest, CreateAccountRequest
 
-from dishes.models import Diner
+from dishes.models import Diner, Chef
 
 @admin.register(CreateAccountRequest)
 class CreateAccountRequestAdmin(admin.ModelAdmin):
@@ -67,13 +69,90 @@ class ChefPermissionsRequestAdmin(admin.ModelAdmin):
     Django ModelAdmin class for providing the Grant/Deny Chef Permissions
     Request functionality.
     """
+    list_display = [
+        "user",
+        "first_dish",
+        "second_dish",
+        "third_dish",
+        "bio",
+    ]
     actions = ["grant_request", "deny_request"]
 
+    def get_queryset(self, request):
+        """
+        Limit the ChefPermissionsRequest queryset to those that are pending.
+        """
+        qs = super(ChefPermissionsRequestAdmin, self).get_queryset(request)
+        return qs.filter(status=ChefPermissionsRequest.PENDING)
+
+    def first_dish(self, chefPermissionsRequest):
+        if chefPermissionsRequest.first_dish_image:
+            url = chefPermissionsRequest.first_dish_image.url
+            head, fname = os.path.split(url)
+            url = os.path.join("/static", fname)
+            return '<a href="{url}">{dish_name}</a>'.format(
+                    url=url,
+                    dish_name=chefPermissionsRequest.first_dish_name
+                )
+        else:
+            return chefPermissionsRequest.first_dish_name
+
+    first_dish.allow_tags = True
+
+    def second_dish(self, chefPermissionsRequest):
+        if chefPermissionsRequest.second_dish_image:
+            url = chefPermissionsRequest.second_dish_image.url
+            head, fname = os.path.split(url)
+            url = os.path.join("/static", fname)
+            return '<a href="{url}">{dish_name}</a>'.format(
+                    url=url,
+                    dish_name=chefPermissionsRequest.second_dish_name
+                )
+        else:
+            return chefPermissionsRequest.second_dish_name
+
+    second_dish.allow_tags = True
+
+    def third_dish(self, chefPermissionsRequest):
+        if chefPermissionsRequest.third_dish_image:
+            url = chefPermissionsRequest.third_dish_image.url
+            head, fname = os.path.split(url)
+            url = os.path.join("/static", fname)
+            return '<a href="{url}">{dish_name}</a>'.format(
+                    url=url,
+                    dish_name=chefPermissionsRequest.third_dish_name
+                )
+        else:
+            return chefPermissionsRequest.third_dish_name
+
+    third_dish.allow_tags = True
+
+    def bio(self, chefPermissionsRequest):
+        if chefPermissionsRequest.video_biography:
+            url = chefPermissionsRequest.video_biography.url
+            head, fname = os.path.split(url)
+            url = os.path.join("/static", fname)
+            return '<a href="{url}">Bio</a>'.format(url=url)
+        else:
+            return "(No Bio found)"
+
+    bio.allow_tags = True
+
+
     def grant_request(self, request, queryset):
-        print("What what what what")
+        for chefPermissionsRequest in queryset:
+            # Create Chef object for this user.
+            user = chefPermissionsRequest.user
+            name = user.first_name + user.last_name
+            chef = Chef.objects.create(user=user)
+            # Update the status of this Chef Permissions Request
+            chefPermissionsRequest.status = ChefPermissionsRequest.APPROVED
+            chefPermissionsRequest.save()
 
     def deny_request(self, request, queryset):
-        print("Yut yut yut yut")
+        for chefPermissionsRequest in queryset:
+            chefPermissionsRequest.status = ChefPermissionsRequest.REJECTED
+            chefPermissionsRequest.save()
 
 @admin.register(RedFlag)
 class RedFlagAdmin(admin.ModelAdmin):
